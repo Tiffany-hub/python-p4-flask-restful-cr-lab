185e9e820f36fdb818800698a052122e504e0285
+ <+>
2023-10-13 14:15:28 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
+
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-from models import SerializerMixin
 
 from models import db, Plant
 
@@ -15,42 +16,55 @@ db.init_app(app)
 
 api = Api(app)
 
-# Define the Plant model as a SerializerMixin
-class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    image = db.Column(db.String(255))
-    price = db.Column(db.Float)
-
-# Define the Routes
-
 class Plants(Resource):
-    def get(self):
-        plants = Plant.query.all()
-        return jsonify([plant.to_dict() for plant in plants])
+     def get(self):
+        
+        response_dict_list = [n.to_dict() for n in Plant.query.all()]
 
-    def post(self):
-        data = request.get_json()
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price']
+        response = make_response(
+            response_dict_list,
+            200,
         )
-        db.session.add(new_plant)
-        db.session.commit()
-        return jsonify(new_plant.to_dict()), 201
+
+        return response
+     def post(self):
+            data = request.get_json()
+            new_record = Plant(
+                name=data['name'],
+                image=data['image'],
+                price=data['price']
+            )
+
+            db.session.add(new_record)
+            db.session.commit()
+
+            response_dict = new_record.to_dict()
+
+            response = make_response(
+                response_dict,
+                201,
+            )
+
+            return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
     def get(self, id):
-        plant = Plant.query.get(id)
-        if plant is None:
-            return make_response(jsonify({'error': 'Not Found'}), 404)
-        return jsonify(plant.to_dict())
 
-api.add_resource(Plants, '/plants')
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+
 api.add_resource(PlantByID, '/plants/<int:id>')
+
+        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    
